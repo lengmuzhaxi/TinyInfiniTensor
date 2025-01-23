8e75c18a9af74cd061cd888cf0a8b99a41f5bcdc
lengmuzhaxi <2690497440@qq.com>
2025-01-23 19:37:31 +0800

    concat算子形状推导

--- src/operators/concat.cc
+++ src/operators/concat.cc
@@ -17,8 +17,23 @@ optional<vector<Shape>> ConcatObj::inferShape(const TensorVec &inputs) {
     // TODO：修改 dims，返回正确的 concat 后的 shape
     // REF: https://onnx.ai/onnx/operators/onnx__Concat.html#concat-13
     // =================================== 作业 ===================================
+      dims[dim] = 0;
+  for (const auto &input : inputs) {
+    if (input->getRank() != rank) {
+      return std::nullopt;
+    }
+    for (size_t i = 0; i < rank; i++) {
+      if (i == static_cast<size_t>(dim)) {
+        dims[i] += input->getDims()[i];
+      } else {
+        if (dims[i] != input->getDims()[i]) {
+          return std::nullopt;
+        }
+      }
+    }
+  }
 
-    return {{dims}};
+  return {{dims}};
 }
 
 std::string ConcatObj::toString() const {
